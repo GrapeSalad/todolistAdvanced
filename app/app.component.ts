2b4d432842f295e808e58a274198c42e033a48b5
David Wilson and kimlan nguyen <david+kimlan@>
2017-07-05 16:24:58 -0700

    add pipe to display completed tasks vs incomplete tasks.

--- app/app.component.ts
+++ app/app.component.ts
@@ -11,6 +11,8 @@ import { Task } from './task.model';
     <task-list [childTaskList]="masterTaskList" (clickSender)="editTask($event)"></task-list>
     <hr>
     <edit-task [childSelectedTask] = "masterSelectedTask" (doneButtonClickedSender) = "finishedEditting()"></edit-task>
+    <hr>
+    <new-task (newTaskSender)="addTask($event)"></new-task>
   </div>
   `
 })
@@ -36,5 +38,9 @@ export class AppComponent {
 
   finishedEditting(){
     this.masterSelectedTask = null;
+  }
+
+  addTask(newTaskFromChild: Task) {
+    this.masterTaskList.push(newTaskFromChild);
   }
 }
